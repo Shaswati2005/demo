3f75a40cbb20f24de8dc177027d1b7db4b768d1f
Shaswati2005 <mishrashaswati2005@gmail.com>
2026-07-17 23:10:50 +0530

    updated ollama config

--- src/main/java/com/example/demo/config/OllamaConfig.java
+++ src/main/java/com/example/demo/config/OllamaConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,11 +8,14 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class OllamaConfig {
 
+    @Value("${ollama.base-url}")
+    private String baseUrl;
+
     @Bean
     public RestClient restClient() {
 
         return RestClient.builder()
-                .baseUrl("http://localhost:11434")
+                .baseUrl(baseUrl)
                 .build();
 
     }
